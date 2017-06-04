d68c5b60748d8adee97df5c7d737fa69a3487c5d
Agarwal <shaleagarwal@deloitte.com>
2017-06-04 19:44:38 +0530

    Commit Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
-
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { NgProgressModule } from 'ng2-progressbar';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    HomePageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AlertModule.forRoot()
+    NgProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
